31f1bce5d1f76971dd08fbf90f132361a2d35c4c
AbdellahAitAbdelmalek <abdellahaa@bam.tech>
2020-11-24 18:59:02 +0100

    first commit of Naza Project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import axios from 'axios';
+
+const Item = ({ item, onPress, style}:any) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.name}</Text>
+  </TouchableOpacity>
+);
 
 export default function App() {
+  const [selectedId, setSelectedId] = useState(null);
+  const [listNearEarthObjects, setlistNearEarthObjects] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const url="http://www.neowsapp.com/rest/v1/neo/browse?page=0&size=20&api_key=y125lgm1Npphd8NEldDxfgTQ5q1NsnCsXzTgjqXw"
+
+  React.useEffect(() => {
+    axios
+    .get(url)
+    .then((res : any) =>{
+        console.log(res.data.near_earth_objects);
+        setlistNearEarthObjects(res.data.near_earth_objects)
+        setisLoading(false);
+    })
+    .catch((err : any)=> {
+      console.log("erreur !!!!");
+    })  
+  },[]);
+  
+  
+  const renderItem = ({ item }:any) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    return <Item item={item} onPress={() => setSelectedId(item.id)} style={{ backgroundColor }} />;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div>
+      {isLoading == true ? 
+        <div>Loading...</div>
+         : 
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={listNearEarthObjects}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            extraData={selectedId}
+          />
+        </SafeAreaView>
+      }
+    </div>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
