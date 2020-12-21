58ba4fdb52b8555f3bef52cfc943713c358b7069
AIT ABDELMALEK Abdellah <abdellahaa@bam.tech>
2020-12-21 13:51:31 +0100

    added jest tests

---
+++ API/NazaApi.tsx
@@ -0,0 +1,2 @@
+const API_TOKEN = "y125lgm1Npphd8NEldDxfgTQ5q1NsnCsXzTgjqXw";
+export default API_TOKEN;
---
+++ metro.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  transformer: {
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+};
