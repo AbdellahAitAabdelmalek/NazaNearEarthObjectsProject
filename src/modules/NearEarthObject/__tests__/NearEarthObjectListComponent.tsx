0e8313145bc8f17a32ee4400ed95b59ec5b25efa
AIT ABDELMALEK Abdellah <abdellahaa@bam.tech>
2020-12-28 09:15:56 +0100

    fixed NearEarthObjectListComponent test with mocked data

--- src/modules/NearEarthObject/__tests__/NearEarthObjectListComponent.tsx
+++ src/modules/NearEarthObject/__tests__/NearEarthObjectListComponent.tsx
@@ -2,34 +2,70 @@ import React from "react";
 import { act, create } from "react-test-renderer";
 import { NearEarthObjectListComponent } from "../components/NearEarthObjectListComponent";
 import { NearEarthObject } from "../types/NearEarthObject.type";
+import nock from "nock";
+
 function tick() {
   return act(
     () =>
       new Promise((resolve) =>
         setTimeout(() => {
           resolve();
-        }, 2)
+        }, 0)
       )
   );
 }
+const scope = nock("http://www.neowsapp.com")
+  .get(
+    "/rest/v1/neo/browse?page=0&size=20&api_key=y125lgm1Npphd8NEldDxfgTQ5q1NsnCsXzTgjqXw"
+  )
+  .reply(200, {
+    near_earth_objects: [
+      {
+        id: 12,
+        neo_reference_id: 13,
+        name: "Abdellah",
+        name_limited: "name_limited",
+        designation: "designation",
+        nasa_jpl_url: "nasa_jpl_url",
+        absolute_magnitude_h: 12,
+        is_potentially_hazardous_asteroid: true,
+        is_sentry_object: false,
+      },
+      {
+        id: 13,
+        neo_reference_id: 19,
+        name: "Djamal Dahman",
+        name_limited: "name_limited2",
+        designation: "designation2",
+        nasa_jpl_url: "nasa_jpl_url2",
+        absolute_magnitude_h: 32,
+        is_potentially_hazardous_asteroid: true,
+        is_sentry_object: false,
+      },
+    ],
+  });
+scope.activeMocks();
 test("renders correctly", async () => {
-  // let tree;
-  // act(() => {
-  //   tree = create(
-  //     <NearEarthObjectListComponent
-  //       onItemIsPressed={(nearEarthObject: NearEarthObject) =>
-  //         console.log(" -- send -- " + nearEarthObject.id)
-  //       }
-  //     />
-  //   ).toJSON();
-  // });
-  const tree = await create(
-    <NearEarthObjectListComponent
-      onItemIsPressed={(nearEarthObject: NearEarthObject) =>
-        console.log(" -- send -- " + nearEarthObject.id)
-      }
-    />
-  ).toJSON();
+  let tree: any;
+  await act(async () => {
+    tree = create(
+      <NearEarthObjectListComponent
+        onItemIsPressed={(nearEarthObject: NearEarthObject) =>
+          console.log(" -- send -- " + nearEarthObject.id)
+        }
+      />
+    );
+  });
+
+  await act(async () => {
+    tree.update(
+      <NearEarthObjectListComponent
+        onItemIsPressed={(nearEarthObject: NearEarthObject) =>
+          console.log(" -- send -- " + nearEarthObject.id)
+        }
+      />
+    );
+  });
   await tick();
 
   expect(tree).toMatchSnapshot();
--- src/modules/NearEarthObject/components/NearEarthObjectListComponent.tsx
+++ src/modules/NearEarthObject/components/NearEarthObjectListComponent.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent, useState } from "react";
 import { NearEarthObject } from "../types/NearEarthObject.type";
-import { ActivityIndicator, FlatList, StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
 import { NearEarthObjectItem } from "./NearEarthObjectItem";
+import axios from "axios";
 
 const url =
   "http://www.neowsapp.com/rest/v1/neo/browse?page=0&size=20&api_key=y125lgm1Npphd8NEldDxfgTQ5q1NsnCsXzTgjqXw";
@@ -16,122 +17,71 @@ export const NearEarthObjectListComponent: FunctionComponent<NearEarthObjectList
   console.log("start - NearEarthObjectListComponent ");
   const [listNearEarthObjects, setlistNearEarthObjects] = useState<
     NearEarthObject[]
-  >([
-    {
-      id: 12,
-      neo_reference_id: 13,
-      name: "Abdel",
-      name_limited: "name_limited",
-      designation: "designation",
-      nasa_jpl_url: "nasa_jpl_url",
-      absolute_magnitude_h: 12,
-      is_potentially_hazardous_asteroid: true,
-      is_sentry_object: false,
-    },
-    {
-      id: 13,
-      neo_reference_id: 19,
-      name: "Djamal",
-      name_limited: "name_limited2",
-      designation: "designation2",
-      nasa_jpl_url: "nasa_jpl_url2",
-      absolute_magnitude_h: 32,
-      is_potentially_hazardous_asteroid: true,
-      is_sentry_object: false,
-    },
-  ]);
+  >([]);
   const [isLoading, setisLoading] = useState<boolean>(true);
 
+  const loadData = async () => {
+    axios
+      .get(url)
+      .then((res) => {
+        const myList: NearEarthObject[] = res.data.near_earth_objects.map(
+          function (near_earth_object: NearEarthObject) {
+            return {
+              id: near_earth_object.id,
+              neo_reference_id: near_earth_object.neo_reference_id,
+              name: near_earth_object.name,
+              name_limited: near_earth_object.name_limited,
+              designation: near_earth_object.designation,
+              nasa_jpl_url: near_earth_object.nasa_jpl_url,
+              absolute_magnitude_h: near_earth_object.absolute_magnitude_h,
+              is_potentially_hazardous_asteroid:
+                near_earth_object.is_potentially_hazardous_asteroid,
+              is_sentry_object: near_earth_object.is_sentry_object,
+            };
+          }
+        );
+        setlistNearEarthObjects(myList);
+        setisLoading(false);
+      })
+      .catch((_err) => {
+        console.log("erreur !!!!" + _err);
+      });
+  };
   React.useEffect(() => {
     console.log("start loading - ");
-    setisLoading(false);
+    loadData();
     console.log("finish loading - ");
-
-    // fetch(url, { method: "GET" })
-    //   .then(function (response) {
-    //     return response.json();
-    //   })
-    //   .then(function (result) {
-    //     const myList: NearEarthObject[] = result.near_earth_objects.map(
-    //       function (near_earth_object: NearEarthObject) {
-    //         return {
-    //           id: near_earth_object.id,
-    //           neo_reference_id: near_earth_object.neo_reference_id,
-    //           name: near_earth_object.name,
-    //           name_limited: near_earth_object.name_limited,
-    //           designation: near_earth_object.designation,
-    //           nasa_jpl_url: near_earth_object.nasa_jpl_url,
-    //           absolute_magnitude_h: near_earth_object.absolute_magnitude_h,
-    //           is_potentially_hazardous_asteroid:
-    //             near_earth_object.is_potentially_hazardous_asteroid,
-    //           is_sentry_object: near_earth_object.is_sentry_object,
-    //         };
-    //       }
-    //     );
-    //     // setlistNearEarthObjects(myList);
-    //     setisLoading(false);
-    //   })
-    //   .catch(function (error) {
-    //     console.log("-------- error ------- " + error);
-    //     alert("result:" + error);
-    //   });
-
-    // axios
-    //   .get(url)
-    //   .then((res) => {
-    //     const myList: NearEarthObject[] = res.data.near_earth_objects.map(
-    //       function (near_earth_object: NearEarthObject) {
-    //         return {
-    //           id: near_earth_object.id,
-    //           neo_reference_id: near_earth_object.neo_reference_id,
-    //           name: near_earth_object.name,
-    //           name_limited: near_earth_object.name_limited,
-    //           designation: near_earth_object.designation,
-    //           nasa_jpl_url: near_earth_object.nasa_jpl_url,
-    //           absolute_magnitude_h: near_earth_object.absolute_magnitude_h,
-    //           is_potentially_hazardous_asteroid:
-    //             near_earth_object.is_potentially_hazardous_asteroid,
-    //           is_sentry_object: near_earth_object.is_sentry_object,
-    //         };
-    //       }
-    //     );
-    //     setlistNearEarthObjects(myList);
-    //     setisLoading(false);
-    //   })
-    //   .catch((_err) => {
-    //     console.log("erreur !!!!" + _err);
-    //   });
   }, []);
 
   return (
     <>
-      {isLoading === true ? (
-        <ActivityIndicator
-          size='large'
-          color='#00ff00'
-          style={styles.activityIndicatorStyle}
-        />
-      ) : (
-        //   (
-        //   listNearEarthObjects?.map((nearEarthObject) => {
-        //     return (
-        //       <NearEarthObjectItem
-        //         key={nearEarthObject.id}
-        //         onItemIsPressed={onItemIsPressed}
-        //         {...nearEarthObject}
-        //       />
-        //     );
-        //   })
-        // )
+      {
+        isLoading === true ? (
+          <ActivityIndicator
+            size='large'
+            color='#00ff00'
+            style={styles.activityIndicatorStyle}
+          />
+        ) : (
+          listNearEarthObjects?.map((nearEarthObject) => {
+            return (
+              <NearEarthObjectItem
+                key={nearEarthObject.id}
+                onItemIsPressed={onItemIsPressed}
+                {...nearEarthObject}
+              />
+            );
+          })
+        )
 
-        <FlatList<NearEarthObject>
-          data={listNearEarthObjects}
-          renderItem={({ item }: { item: NearEarthObject }) => (
-            <NearEarthObjectItem onItemIsPressed={onItemIsPressed} {...item} />
-          )}
-          keyExtractor={(item: NearEarthObject) => item.id.toString()}
-        />
-      )}
+        // <FlatList<NearEarthObject>
+        //   data={listNearEarthObjects}
+        //   renderItem={({ item }: { item: NearEarthObject }) => (
+        //     <NearEarthObjectItem onItemIsPressed={onItemIsPressed} {...item} />
+        //   )}
+        //   keyExtractor={(item: NearEarthObject) => item.id.toString()}
+        // />)
+      }
     </>
   );
 };
